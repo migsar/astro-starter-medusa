5e9ccf32509067e2b63ea3ab24245aa4a9722447
Migsar Navarro <migsar.navarro@gmail.com>
2023-02-28 02:40:52 +0000

    chore: add astro image

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,17 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    image(),
+  ],
   vite: {
     resolve: {
       alias: {
