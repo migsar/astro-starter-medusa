7969b5c32f3309a5180773bd4c2215bcb081ff40
Migsar Navarro <migsar.navarro@gmail.com>
2023-02-28 04:01:36 +0000

    chore: add daisyui

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,10 @@
+const daisyui = require('daisyui');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [ daisyui ],
 }
