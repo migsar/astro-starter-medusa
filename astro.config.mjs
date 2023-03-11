9fdf7d038fc165a90627e74de05ce525848fd0bf
Migsar Navarro <migsar.navarro@gmail.com>
2023-03-11 20:43:58 +0000

    chore: set dev server port to 8000 to use medusa standard

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,9 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
+  server: {
+    port: 8000,
+  },
   integrations: [
     tailwind(),
     image(),
